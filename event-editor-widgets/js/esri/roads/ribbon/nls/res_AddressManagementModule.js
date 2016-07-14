d54af968fa395d4e4a63c71c3cc3a8cdda6e35f1
Tom Hill <tomhill@esri.com>
2016-07-14 15:22:13 -0700

    added string resources

---
+++ event-editor-widgets/js/esri/roads/ribbon/nls/res_AddressManagementModule.js
@@ -0,0 +1,7 @@
+define({root:{
+    featureLayerNotDetected: "No feature service detected.",
+    loading: "Loading...",
+    loadConfig: "There is an error reading 'address_management.json' file. Addressing widgets wll not load until the error is fixed.<br/>Please notify the application administrator.",
+    loadFeatureLayer:"Layers configured for Address Management could not be located in the configured Map Service(s). Please check the map service(s) if the layers are present and/or check the address management configuration file to see if the layer names are correct.",
+    addressRangeFieldsError: "This widget requires address range fields to be of numeric type. Please update the following fields to be numeric field type."
+}});
